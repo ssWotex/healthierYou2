f22b89dcc4040452553214fed0b337061810e21d
DODoubleDizzle <doriano.dipierro@bwz-rappi.ch>
2020-08-25 14:14:03 +0200

    MainMenu

--- app/src/main/java/com/example/healthieryou/MenuActivity.java
+++ app/src/main/java/com/example/healthieryou/MenuActivity.java
@@ -2,13 +2,49 @@ package com.example.healthieryou;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
+    Button btnSprint;
+    Button btnRun;
+    Button btnStep;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        btnSprint = findViewById(R.id.btnSprint);
+        btnRun = findViewById(R.id.btnRun);
+        btnStep = findViewById(R.id.btnStep);
+
+        btnSprint.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SprintTestActivity.class);
+                startActivity(intent);
+            }
+        });
+        btnRun.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MapMyRunActivity.class);
+                startActivity(intent);
+            }
+        });
+        btnStep.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), StepCounterActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
+
 }
